d16bf0129a73c483e5b2a59a369061126866e4cf
fouzaw <fouzi.lezzar@univ-constantine2.dz>
2025-10-16 17:44:03 +0100

    commit init

--- src/test/java/com/example/demoTest/DemoTestApplicationTests.java
+++ src/test/java/com/example/demoTest/DemoTestApplicationTests.java
@@ -2,15 +2,18 @@ package com.example.demoTest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-
 class DemoTestApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	// Optionnel : ajoutez ce test pour vérifier la config
+    @Test
+    void testProfileIsActive() {
+        System.out.println("✅ Profil 'test' activé – si vous voyez ce message, la config H2 est utilisée.");
+    }
 
 }
