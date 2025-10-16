8101bea66b4c64e6a18ea22a08838dea23ce7b36
fouzaw <fouzi.lezzar@univ-constantine2.dz>
2025-10-16 17:07:21 +0100

    Init Commit

---
+++ src/test/java/com/example/demoTest/DemoTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+
+class DemoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
